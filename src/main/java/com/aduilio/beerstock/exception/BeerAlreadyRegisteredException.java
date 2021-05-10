cbf1af332f50ce84b35453c7d61d57504bbd930e
Aduilio Silva <developer.aduilio@gmail.com>
2021-05-10 15:57:16 -0300

    Add exception error code

--- src/main/java/com/aduilio/beerstock/exception/BeerAlreadyRegisteredException.java
+++ src/main/java/com/aduilio/beerstock/exception/BeerAlreadyRegisteredException.java
@@ -1,9 +1,13 @@
 package com.aduilio.beerstock.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Thrown to indicate that the application has attempted to create a beer with a
  * name that already exists.
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BeerAlreadyRegisteredException extends Exception {
 
 	private static final long serialVersionUID = 8486505345635843445L;
--- src/main/java/com/aduilio/beerstock/exception/BeerNotFoundException.java
+++ src/main/java/com/aduilio/beerstock/exception/BeerNotFoundException.java
@@ -1,9 +1,13 @@
 package com.aduilio.beerstock.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Thrown to indicate that the application has attempted to read a beer that
  * does not exist.
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BeerNotFoundException extends Exception {
 
 	private static final long serialVersionUID = -5453365429772776167L;
